632a35b51b7cae4f7664a0d06aadd97c8a9da355
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2021-03-11 15:19:57 +0100

    Adds some documentation

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Microsoft Graph API Demo App
+
+Die Anwendung nutzt die Microsoft Authentication Library für Java (MSAL4J)
+
+## Setup
+1. Anwendung unter [Azure portal](https://portal.azure.com) registrieren
+1. Client Secret anlegen
+1. application.properties mit den Daten aus der registrierten App anpassen
+
+```
+# src/main/resources/application.properties
+AUTHORITY=https://login.microsoftonline.com/<TENANT_ID>/
+CLIENT_ID=<CLIENT_ID>
+SECRET=<CLIENT_SECRET>
+SCOPE=https://graph.microsoft.com/.default
+FOLDER_ID=
+USER_ID=
+```
+
+MailboxDemo App starten.
--- src/main/java/de/codekeepers/MailboxDemo.java
+++ src/main/java/de/codekeepers/MailboxDemo.java
@@ -1,5 +1,4 @@
-package de.codekeepers;// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+package de.codekeepers;
 
 import com.microsoft.aad.msal4j.ClientCredentialFactory;
 import com.microsoft.aad.msal4j.ClientCredentialParameters;
@@ -27,19 +26,18 @@ class MailboxDemo {
     private static String clientId;
     private static String secret;
     private static String scope;
-
-    private static final String FOLDER_ID_CRM = "AAMkADljYTg5ZDAxLWFhN2YtNDgxMi05MmU4LTQ1YjQ1ZGQ0NzdmNQAuAAAAAADRfUy7cPAQRr4pRB9mHHNzAQDYo6CezcgySaauGw-KNAd0AACIxhCMAAA=";
-    public static final String USER_ID_RW = "591dfef2-9a07-48db-8ffe-4cc4e18af791";
+    private static String folderId;
+    private static String userId;
 
     public static void main(String args[]) throws Exception {
         setUpSampleData();
         try {
             IAuthenticationResult result = getAccessTokenByClientCredentialGrant();
-            List<Folder> folders = getFolders(USER_ID_RW, result.accessToken());
+            List<Folder> folders = getFolders(userId, result.accessToken());
             for (Folder f : folders) {
                 System.out.println(f.id + ": " + f.displayName);
             }
-            List<Mail> mails = getMails("591dfef2-9a07-48db-8ffe-4cc4e18af791", FOLDER_ID_CRM, result.accessToken());
+            List<Mail> mails = getMails("591dfef2-9a07-48db-8ffe-4cc4e18af791", folderId, result.accessToken());
             for (Mail m : mails) {
                 System.out.println(m.subject);
                 System.out.println(m.body.content);
@@ -53,7 +51,6 @@ class MailboxDemo {
     }
 
     private static IAuthenticationResult getAccessTokenByClientCredentialGrant() throws Exception {
-
         ConfidentialClientApplication app = ConfidentialClientApplication.builder(
                 clientId,
                 ClientCredentialFactory.createFromSecret(secret))
@@ -70,66 +67,6 @@ class MailboxDemo {
         return future.get();
     }
 
-    private static String getUsersListFromGraph(String accessToken) throws IOException {
-        URL url = new URL("https://graph.microsoft.com/v1.0/users");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Authorization", "Bearer " + accessToken);
-        conn.setRequestProperty("Accept", "application/json");
-
-        int httpResponseCode = conn.getResponseCode();
-        if (httpResponseCode == HTTPResponse.SC_OK) {
-
-            StringBuilder response;
-            try (BufferedReader in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()))) {
-
-                String inputLine;
-                response = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-            }
-            return response.toString();
-        } else {
-            return String.format("Connection returned HTTP code: %s with message: %s",
-                    httpResponseCode, conn.getResponseMessage());
-        }
-    }
-
-    private static String getMailsFromGraph(String accessToken) throws IOException {
-        URL url = new URL("https://graph.microsoft.com/v1.0/users/591dfef2-9a07-48db-8ffe-4cc4e18af791/mailFolders");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Authorization", "Bearer " + accessToken);
-        conn.setRequestProperty("Accept", "application/json");
-
-        int httpResponseCode = conn.getResponseCode();
-        if (httpResponseCode == HTTPResponse.SC_OK) {
-
-            StringBuilder response;
-            try (BufferedReader in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()))) {
-
-                String inputLine;
-                response = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    System.out.println(inputLine);
-                }
-            }
-            return response.toString();
-        } else {
-            return String.format("Connection returned HTTP code: %s with message: %s",
-                    httpResponseCode, conn.getResponseMessage());
-        }
-    }
-
-    /**
-     * Helper function unique to this sample setting. In a real application these wouldn't be so hardcoded, for example
-     * different users may need different authority endpoints or scopes
-     */
     private static void setUpSampleData() throws IOException {
         // Load properties file and set properties used throughout the sample
         Properties properties = new Properties();
@@ -138,5 +75,7 @@ class MailboxDemo {
         clientId = properties.getProperty("CLIENT_ID");
         secret = properties.getProperty("SECRET");
         scope = properties.getProperty("SCOPE");
+        folderId = properties.getProperty("FOLDER_ID");
+        userId = properties.getProperty("USER_ID");
     }
 }
